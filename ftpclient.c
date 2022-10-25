c3597f8966991877a0bbabbdc97055f4d6fc6a10
张涵星 <zhanghanxing@zhanghanxings-MacBook-Air.local>
2022-10-25 14:21:40 +0800

    first commit

---
+++ ftpclient.c
@@ -0,0 +1,50 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+ 
+#define SERVER_PORT  5050
+#define SERVER_IP    "127.0.0.1"
+ 
+int main(int argc, char *argv[])
+{
+    //创建客户端套接字号，并检测是否创建成功
+    int sockCli;
+    sockCli = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockCli == -1)
+        perror("socket");
+ 
+    //创建一个地址信息结构体，并对其内容进行设置
+    struct sockaddr_in addrSer,addrCli;
+    addrSer.sin_family = AF_INET;         //使用AF_INET协议族
+    addrSer.sin_port = htons(SERVER_PORT);  //设置端口号
+    addrSer.sin_addr.s_addr = inet_addr(SERVER_IP);   //设置服务器ip
+ 
+    bind(sockCli,(struct sockaddr*)&addrCli, sizeof(struct sockaddr));    //将套接字地址与所创建的套接字号联系起来
+ 
+    //创建一个与服务器的连接，并检测连接是否成功
+    socklen_t addrlen = sizeof(struct sockaddr);
+    int res = connect(sockCli,(struct sockaddr*)&addrSer, addrlen);
+    if(res == -1)
+        perror("connect");
+    else
+        printf("Client Connect Server OK.\n");
+ 
+    char sendbuf[256];     //申请一个发送数据缓存区
+    char recvbuf[256];     //申请一个接收数据缓存区
+    while(1)
+    {
+        recv(sockCli, recvbuf, 256, 0);    //接收来自服务器的数据
+        printf("Ser:> %s\n",recvbuf);
+        printf("Cli:>");
+        scanf("%[^\n]",sendbuf);
+        fflush(stdin);
+        if(strncmp(sendbuf,"quit", 4) == 0)    //如果客户端发送的数据为"quit"，则退出。
+            break;
+        send(sockCli, sendbuf, strlen(sendbuf)+1, 0);   //发送数据
+    }
+    close(sockCli);       //关闭套接字
+    return 0;
+}
---
+++ ftpserver.c
@@ -0,0 +1,73 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#define SERBER_PORT 5050
+#define SERVER_IP "127.0.0.1"
+#define QUEUE_SIZE 5
+
+
+#pragma pack(1) // 设置结构体1字节对齐**************
+
+struct MsgHeader
+{
+    /* data */
+};
+#pragma pack()
+
+int main(int argc, char *argv[]){
+    int sockSer;
+    sockSer = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockSer == -1){
+        perror("socket");
+    }
+
+    int yes = 1;
+    if(setsockopt(sockSer, SOL_SOCKET, SO_REUSEADDR,&yes,sizeof(int))==-1){
+        perror("socket");
+    }
+
+    struct sockaddr_in addrSer,addrCli;
+    addrSer.sin_family = AF_INET;
+    addrSer.sin_port = htons(SERBER_PORT);
+    addrSer.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    socklen_t addrlen = sizeof(struct sockaddr);
+    int res = bind(sockSer,(struct sockaddr*)&addrSer,addrlen);
+    if(res == -1){
+        perror("bind");
+    }
+
+    listen(sockSer, QUEUE_SIZE);
+
+    printf("Server Wait Client Accept………\n ");
+
+    int sockConn = accept(sockSer, (struct sockaddr*)&addrCli, &addrlen);
+    if(sockConn == -1)
+        perror("accept");
+    else{
+        printf("Server Accept Clinet OK.\n");
+        printf("Client IP:> %s\n", inet_ntoa(addrCli.sin_addr));
+        printf("Client Port:> %d\n", ntohs(addrCli.sin_port));
+    }
+
+    char sendbuf[256];
+    char recvbuf[256];
+    while (1)
+    {
+        printf("Ser:>");
+        scanf("%[^\n]",sendbuf);
+        fflush(stdin);
+        if(strncmp(sendbuf, "quit", 4) ==0)
+            break;
+        send(sockConn, sendbuf, strlen(sendbuf)+1, 0);
+        recv(sockConn, recvbuf, 256, 0);
+        printf("Cli:> %s\n", recvbuf);
+
+    }
+    close(sockSer);
+    return 0;
+}
